4c8b27588e9e49642b773d60e0fc7a309392aeb6
RudeevK161 <90448797+RudeevK161@users.noreply.github.com>
2021-10-26 21:34:02 +0300

    Add files via upload

---
+++ js7.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".gallery").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 850,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            }
+          ]
+        
+      });
+  });
+© 2021 GitHub, Inc.
